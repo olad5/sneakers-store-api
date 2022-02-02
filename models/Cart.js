1fea39075aafb764d0d2aaebbaee634a9f9f5b70
Francis Shonubi <shonubioladipo@gmail.com>
2022-02-02 11:21:45 +0100

    feat: add shopping Cart model

---
+++ models/Cart.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+
+const CartItemSchema = new mongoose.Schema({
+  itemId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Item",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [1, 'Quantity can not be less then 1.']
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  total: {
+    type: Number,
+    required: true,
+  }
+}, {
+  timestamps: true
+})
+
+const CartSchema = new mongoose.Schema({
+
+  items: [CartItemSchema],
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Cart must belong to a User!']
+  },
+
+  subTotal: {
+    default: 0,
+    type: Number
+  }
+}, {
+  timestamps: true
+})
+
+
+
+const Cart = mongoose.model('Cart', CartSchema);
+
+export default Cart;
