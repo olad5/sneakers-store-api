6d761910cc44479e06e81fa65d4d4e6312cfaf23
Francis Shonubi <shonubioladipo@gmail.com>
2022-02-09 14:09:30 +0100

    fix: modfied auth to include user's email
    - authentication utils now shows user's email. This is useful for
      including email addresses in users orders.

--- utils/createTokenUser.js
+++ utils/createTokenUser.js
@@ -1,6 +1,6 @@
 // creates a token user so that the jwt authentication can be added to that user and cookies generated for that user
 const createTokenUser = (user) => {
-  return {name: user.name, userId: user._id, role: user.role};
+  return {name: user.name, userId: user._id, email: user.email, role: user.role};
 };
 
 export default createTokenUser;
