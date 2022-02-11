20d115c506159836ddcf6925d2e063dce318eeff
Francis Shonubi <shonubioladipo@gmail.com>
2022-02-11 18:49:53 +0100

    feat: add security packages

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import logger from 'morgan'
+import rateLimiter from 'express-rate-limit'
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
+import compression from 'compression'
+import hpp from 'hpp'
+
 import itemRouter from './routes/itemRoutes.js'
 import userRouter from './routes/userRoutes.js'
 import authRouter from './routes/authRoutes.js'
@@ -11,14 +18,42 @@ import cartRouter from './routes/cartRoutes.js'
 import orderRouter from './routes/orderRoutes.js'
 import checkoutRouter from './routes/checkoutRoutes.js'
 
-
+// Start express app
 const app = express()
 dotenv.config()
 app.use(logger('dev'))
 
-app.use(bodyParser.json({limit: '30mb', extended: true}))
+// Limit requests from same API
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+
+app.use(bodyParser.json({limit: '31mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
-app.use(cors())
+app.use(helmet());// Set security HTTP headers
+app.use(cors());// Implement CORS
+app.use(xss()); // Data sanitization against XSS
+app.use(mongoSanitize());// Data sanitization against NoSQL query injection
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+app.use(compression());
+
 app.use(cookieParser(process.env.JWT_SECRET));
 
 
